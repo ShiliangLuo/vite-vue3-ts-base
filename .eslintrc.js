464a39e39096cc4cc83b826accd6936411881f8b
ShiliangLuo <15151983987@163.com>
2022-01-20 13:40:01 +0800

    chore: 配置eslint提示等级

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,5 +24,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error'],
   },
 }
